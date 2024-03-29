3a2c710593e68e4b33d58a7a2eb5eeb4eac75669
황동현 <106989359+bronzehyeon@users.noreply.github.com>
2024-03-30 08:24:47 +0900

    Add files via upload

---
+++ src/main/java/com/OnedayOwner/server/OnedayOwnerServerApplication.java
@@ -0,0 +1,13 @@
+package com.OnedayOwner.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedayOwnerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnedayOwnerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OnedayOwner/server/OnedayOwnerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnedayOwner.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnedayOwnerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
