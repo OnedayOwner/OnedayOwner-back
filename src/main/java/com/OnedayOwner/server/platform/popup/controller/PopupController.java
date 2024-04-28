8e3394cf60bea88b2373837f9ba23fbd38dde52d
bronzehyeon <bronzehyeon@gmail.com>
2024-04-28 23:00:36 +0900

    custom 레포지토리 생성 및 메서드 생성

---
+++ src/main/java/com/OnedayOwner/server/platform/popup/controller/PopupController.java
@@ -0,0 +1,2 @@
+package com.OnedayOwner.server.platform.popup.controller;public class PopupController {
+}
---
+++ src/main/java/com/OnedayOwner/server/platform/popup/repository/PopupRestaurantRepositoryImpl.java
@@ -0,0 +1,2 @@
+package com.OnedayOwner.server.platform.popup.repository;public class PopupRestaurantRepositoryImpl {
+}
