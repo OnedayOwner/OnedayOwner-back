81aa19b7233d368d7bf2915602d2e58a694439b7
bronzehyeon <bronzehyeon@gmail.com>
2024-09-21 15:27:13 +0900

    - popup api 설명 추가

--- src/main/java/com/OnedayOwner/server/platform/popup/controller/PopupController.java
+++ src/main/java/com/OnedayOwner/server/platform/popup/controller/PopupController.java
@@ -4,6 +4,7 @@ import com.OnedayOwner.server.global.exception.BusinessException;
 import com.OnedayOwner.server.global.security.SecurityUtils;
 import com.OnedayOwner.server.platform.popup.dto.PopupDto;
 import com.OnedayOwner.server.platform.popup.service.PopupService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ public class PopupController {
 
     private final PopupService popupService;
 
+    @Operation(summary = "팝업 등록")
     @PostMapping("/register")
     public ResponseEntity<PopupDto.PopupInBusinessDetail> registerPopup(
         @RequestBody PopupDto.PopupRestaurantForm form,
@@ -30,6 +32,7 @@ public class PopupController {
             .body(popupService.registerPopup(form, ownerId));
     }
 
+    @Operation(summary = "팝업 메뉴 등록")
     @PostMapping("/{popupId}/menu")
     public ResponseEntity<PopupDto.MenuDetail> registerMenu(
         PopupDto.MenuForm form,
@@ -39,6 +42,9 @@ public class PopupController {
             .body(popupService.registerMenu(form, popupId));
     }
 
+    @Operation(summary = "팝업 조회",
+            description = "현재 진행중인 팝업 조회," +
+                    "조회 시점에 팝업 종료됐을 시 close 처리됨")
     @GetMapping("")
     public ResponseEntity<PopupDto.PopupInBusinessDetail> getPopupInBusiness(
             SecurityContextHolderAwareRequestWrapper request
@@ -48,6 +54,8 @@ public class PopupController {
             .body(popupService.getPopupInBusinessDetail(ownerId));
     }
 
+    @Operation(summary = "과거 팝업 상세 조회",
+            description = "과거 진행 팝업 list에서 팝업 선택 시 세부정보(detail) qksghks")
     @GetMapping("/history/{popupId}")
     public ResponseEntity<PopupDto.PopupHistoryDetail> getPopupDetail(
             @PathVariable("popupId")Long popupId
@@ -56,6 +64,8 @@ public class PopupController {
             .body(popupService.getPopupHistoryDetail(popupId));
     }
 
+    @Operation(summary = "과거 팝업 조회",
+            description = "과거 진행 팝업 summary 정보 반환")
     @GetMapping("/popup/history/list")
     public ResponseEntity<List<PopupDto.PopupSummary>> getPopupHistory(
             SecurityContextHolderAwareRequestWrapper request
@@ -72,6 +82,7 @@ public class PopupController {
      * @param popupId
      * @return
      */
+    @Operation(summary = "팝업 삭제")
     @PostMapping("/{popupId}/delete")
     public ResponseEntity<?> deletePopup(
             SecurityContextHolderAwareRequestWrapper request,
@@ -86,6 +97,8 @@ public class PopupController {
         }
     }
 
+    @Operation(summary = "월 날짜별 예약 정보",
+            description = "팝업 정보에서 예약 정보 조회 시 월 날짜별 예약수, 예약인원수 반환")
     @PostMapping("/{popupId}/reservation/month")
     public ResponseEntity<List<PopupDto.ReservationInfoForOwnerSummary>> monthlyReservationInfo(
             SecurityContextHolderAwareRequestWrapper request,
@@ -98,6 +111,8 @@ public class PopupController {
                 .body(popupService.monthlyReservationInfo(ownerId, popupId, year, month));
     }
 
+    @Operation(summary = "날짜 시간별 메뉴 수량 조회",
+            description = "예약 조회 달력에서 날짜 선택 시 해당 날짜의 시간별 메뉴 수량 반환")
     @PostMapping("/{popupId}/reservation/day")
     public ResponseEntity<List<PopupDto.ReservationMenuCount>> dailyMenuCount(
         SecurityContextHolderAwareRequestWrapper request,
